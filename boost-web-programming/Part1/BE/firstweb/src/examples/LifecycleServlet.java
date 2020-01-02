a357dc2704588b8c9571cc007ccbe80fece06605
Seunghyun Yang <ggyool@naver.com>
2020-01-02 17:38:34 +0900

    -

---
+++ boost-web-programming/Part1/BE/firstweb/src/examples/LifecycleServlet.java
@@ -0,0 +1,63 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LifecycleServlet")
+public class LifecycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LifecycleServlet() {
+    	System.out.println("LifecycleServlet constructor");
+    }
+    
+    @Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init run!");
+	}
+    
+	@Override
+	public void destroy() {
+		System.out.println("destroy run");
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service run");
+		super.service(request, response);
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet run");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifecycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+}
