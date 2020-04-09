2913aee847e3b84581f9bc674925bd6e246f2ead
seunghyun <ggyool@naver.com>
2020-04-10 02:13:00 +0900

    -

---
+++ design-pattern/chapter1/src/practice/Acount.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Acount {
+	private Integer id;
+	private String owner;
+	private Double balance;
+	public void deposit(Double amount) {
+		
+	}
+	public void withdraw(Double amount) {
+		
+	}
+	protected Double getBalance() {
+		return this.balance;
+	}
+}
--- ps/boj/10973.cpp
+++ ps/boj/10973.cpp
@@ -9,24 +9,14 @@ void swap(int &refa, int &refb){
     refb = tmp;
 }
 
-// 모든것을 내림차순으로 바꿔야 한다.5
-// 1. v[i]-v[i-1]>0 인 위치를 뒤에서 부터 찾는다.
-// 2. v[i]~끝 중 v[i-1] 보다 큰 다음 숫자를 찾는다. (v[i]가 v[i-1] 보다 크기 때문에 무조건 있다.) 
-// 뒤에서 부터 찾으면 오름차순으로 찾는 것이다. 
-// 3. i-1 과 찾은 값을 바꾼다.
-// 4. 나머지를 뒤집는다.
-bool nextPermutation(vector<int> &v){
+bool prevPermutation(vector<int> &v){
     int len = v.size();
     for(int i=len-1; i>=1; --i){
-        if(v[i]-v[i-1] > 0){
+        if(v[i-1]-v[i]>0){
             for(int j=len-1; j>=i; --j){
-                if(v[j]>v[i-1]){
-                    swap(v[i-1], v[j]);
+                if(v[j]<v[i-1]){
+                    swap(v[j], v[i-1]);
                     reverse(v.begin()+i, v.end());
-                    // int k=i, l=len-1;
-                    // while(k<l){
-                    //     swap(v[k++], v[l--]);
-                    // }
                     return true;
                 }
             }
@@ -40,14 +30,14 @@ int main(void){
     cin.tie(0);
     cout.tie(0);
     int n;
-    vector<int> v;
+    vector<int> v;  
     cin >> n;
     for(int i=0; i<n; ++i){
         int num;
         cin >> num;
         v.push_back(num);
     }
-    if(!nextPermutation(v)) cout << -1;
+    if(!prevPermutation(v)) cout << -1;
     else{
         for(int i=0; i<n; ++i){
             cout << v[i] << ' ';
@@ -55,3 +45,4 @@ int main(void){
     }
     return 0;
 }
+
