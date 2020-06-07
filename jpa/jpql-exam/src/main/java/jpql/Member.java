b28f1c33b416593920620ecb8c2cde206836f51a
seunghyun <ggyool@naver.com>
2020-06-08 03:46:04 +0900

    -

--- jpa/jpql-exam/src/main/java/jpql/Member.java
+++ jpa/jpql-exam/src/main/java/jpql/Member.java
@@ -14,10 +14,17 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="TEAM_ID")
     private Team team;
 
+    @Enumerated(EnumType.STRING)
+    private MemberType type;
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
     public Long getId() {
         return id;
     }
@@ -48,5 +55,23 @@ public class Member {
 
     public void setTeam(Team team) {
         this.team = team;
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                ", teamName=" + team.getName() +
+                '}';
     }
 }
--- jpa/jpql-exam/src/main/java/jpql/jpaMain.java
+++ jpa/jpql-exam/src/main/java/jpql/jpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class jpaMain {
@@ -13,17 +14,60 @@ public class jpaMain {
         tx.begin();
 
         try{
+
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
+            Team teamC = new Team();
+            teamC.setName("teamC");
+            em.persist(teamC);
+
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
+            member.setTeam(teamA);
+            member.setType(MemberType.ADMIN);
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setAge(20);
+            member2.setTeam(teamB);
+            member2.setType(MemberType.USER);
+            em.persist(member2);
 
-            Member findMember = em.createQuery("select m from Member as m where m.username = ?1", Member.class)
-                    .setParameter(1, "member1")
-                    .getSingleResult();
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setAge(60);
+            member3.setTeam(teamC);
+            member3.setType(MemberType.ADMIN);
+            em.persist(member3);
 
-            System.out.println(findMember.getUsername());
+            em.flush();
+            em.clear();
+
+
+            String query = "select m.username from Team t join t.members m";
+
+            List<Collection> result = em.createQuery(query, Collection.class)
+                    .getResultList();
+
+            for(int i=0; i<result.size(); ++i){
+                System.out.println(result.get(0));
+            }
+            // 아래 처럼 탐색하면 안되는데 왜인지는 모르겠다.
+//            for (Collection collection : result) {
+//                System.out.println((Member)collection);;
+//            }
+
+
+
+
 
 
             tx.commit();
