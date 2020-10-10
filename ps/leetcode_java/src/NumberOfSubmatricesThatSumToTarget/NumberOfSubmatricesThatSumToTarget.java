64df4eb73fdba14265bff06afb4026aae5049fa4
seunghyun <ggyool@naver.com>
2020-10-10 17:53:05 +0900

    -

--- ps/leetcode_java/src/NumberOfSubmatricesThatSumToTarget/NumberOfSubmatricesThatSumToTarget.java
+++ ps/leetcode_java/src/NumberOfSubmatricesThatSumToTarget/NumberOfSubmatricesThatSumToTarget.java
@@ -1,35 +1,32 @@
 package NumberOfSubmatricesThatSumToTarget;
 
-public class NumberOfSubmatricesThatSumToTarget {
+import java.util.HashMap;
 
+public class NumberOfSubmatricesThatSumToTarget {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
         int r = matrix.length;
         int c = matrix[0].length;
+        // psum[i][j] = matrix[i][0]~matrix[i][j] 의 합
         int[][] psum = new int[r][c];
-        psum[0][0] = matrix[0][0];
-        for(int j=1; j<c; ++j){
-            psum[0][j] = psum[0][j-1] + matrix[0][j];
-        }
-        for(int i=1; i<r; ++i){
-            psum[i][0] = matrix[i][0] + psum[i-1][0];
-            for(int j=1; j<c; ++j){
-                psum[i][j] = psum[i-1][j] + psum[i][j-1] - psum[i-1][j-1] + matrix[i][j];
+        for(int i=0; i<r; ++i){
+            for(int j=0; j<c; ++j){
+                 if(j==0) psum[i][j] = matrix[i][j];
+                 else psum[i][j] = psum[i][j-1] + matrix[i][j];
             }
         }
         int ans = 0;
-        for(int y=0; y<r; ++y){
-            for(int x=0; x<c; ++x){
-                for(int i=y; i<r; ++i){
-                    for(int j=x; j<c; ++j){
-                        int res = psum[i][j];
-                        if(y-1>=0) res -= psum[y-1][j];
-                        if(x-1>=0) res -= psum[i][x-1];
-                        if(x-1>=0 && y-1>=0)res += psum[y-1][x-1];
-                        if(res==target) {
-                            ++ans;
-                        }
-
+        // 가로 s~f, 세로 i
+        for(int s=0; s<c; ++s){
+            for(int f=s; f<c; ++f){
+                HashMap<Integer, Integer> mp = new HashMap<>();
+                int sum = 0;
+                mp.put(0, 1);
+                for(int i=0; i<r; ++i){
+                    sum += (psum[i][f] - psum[i][s] + matrix[i][s]);
+                    if(mp.containsKey(sum-target)){
+                        ans += mp.get(sum-target);
                     }
+                    mp.put(sum, mp.getOrDefault(sum, 0) + 1);
                 }
             }
         }
