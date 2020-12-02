45c44983179ea55774db8c369990cedf8ce6d9ef
seunghyun <ggyool@naver.com>
2020-12-02 21:39:26 +0900

    -

---
+++ ps/leetcode_java/src/Subsets/Subsets.java
@@ -0,0 +1,28 @@
+package Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Subsets {
+    List<List<Integer>> ret;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        ret = new ArrayList<>();
+        pick(0, nums, new ArrayList<>());
+        return ret;
+    }
+
+    private void pick(int idx, int[] nums, List<Integer> subset) {
+        if (idx == nums.length) {
+            ret.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[idx]);
+        pick(idx + 1, nums, subset);
+        subset.remove(subset.size() - 1);
+
+        pick(idx + 1, nums, subset);
+    }
+}
