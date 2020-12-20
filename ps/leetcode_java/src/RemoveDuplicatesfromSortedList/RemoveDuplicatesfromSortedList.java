a628ac8ccf5086eed65bddf0bc15f41285b4a505
seunghyun <ggyool@naver.com>
2020-12-20 19:49:24 +0900

    -

---
+++ ps/leetcode_java/src/RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,15 @@
+package RemoveDuplicatesfromSortedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class RemoveDuplicatesfromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+    }
+}
