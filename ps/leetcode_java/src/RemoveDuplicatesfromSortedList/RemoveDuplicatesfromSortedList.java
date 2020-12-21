c7c681cbaaf9756c013bafdb2135eb693e99afb0
seunghyun <ggyool@naver.com>
2020-12-21 23:49:49 +0900

    -

--- ps/leetcode_java/src/RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.java
+++ ps/leetcode_java/src/RemoveDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.java
@@ -1,5 +1,8 @@
 package RemoveDuplicatesfromSortedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class ListNode {
     int val;
     ListNode next;
@@ -10,6 +13,18 @@ class ListNode {
 
 public class RemoveDuplicatesfromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
-        
+        Set<Integer> st = new HashSet<>();
+        ListNode bef = null;
+        ListNode cur = head;
+        while (cur != null) {
+            if (!st.contains(cur.val)) {
+                st.add(cur.val);
+                bef = cur;
+            } else {
+                bef.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return head;
     }
 }
