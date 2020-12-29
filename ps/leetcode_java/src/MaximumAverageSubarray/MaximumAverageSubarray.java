5b78e409b4d01ee92a60f5ea4167727cb97eb45a
seunghyun <ggyool@naver.com>
2020-12-29 23:50:24 +0900

    -

---
+++ ps/leetcode_java/src/MaximumAverageSubarray/MaximumAverageSubarray.java
@@ -0,0 +1,17 @@
+package MaximumAverageSubarray;
+
+public class MaximumAverageSubarray {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum -= nums[i - k];
+            sum += nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+    }
+}
